78649a64372fa47f63bffc18191326c76ffb062c
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-28 14:05:45 -0400

    Created GameSettingForm and added the main parts of Main page

---
+++ src/components/mainPage/GameSettingForm.jsx
@@ -0,0 +1,8 @@
+
+function GameSettingForm() {
+  return (
+    <div>GameSettingForm</div>
+  )
+}
+
+export default GameSettingForm
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,12 @@
-
+import GameSettingForm from "../components/mainPage/GameSettingForm"
 function Main() {
   return (
-    <div>Main</div>
+    <div id="main-page">
+        <h1>Arithmetic Game</h1>
+        <p>The Arithmetic Game is a fast-paced speed drill where you are given two minutes to solve as many arithmetic problems as you can.</p>
+        <GameSettingForm />
+
+    </div>
   )
 }
 
